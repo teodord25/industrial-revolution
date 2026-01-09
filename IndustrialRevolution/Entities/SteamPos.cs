0dc9593272b2d17c89d65689cd7f4a7424652e31
teodord25 <djuric.teodor25@gmail.com>
2026-01-09 20:24:09 +0100

    feat(expansion): add grid to steampost

--- IndustrialRevolution/Entities/SteamPos.cs
+++ IndustrialRevolution/Entities/SteamPos.cs
@@ -1,13 +1,26 @@
 using Vintagestory.API.MathTools;
 
+using System.Linq;
+
 namespace IndustrialRevolution.Entities;
 public class SteamPos : BlockPos
 {
     public bool isFullBlock;
 
+    public byte[,,]? grid;
+
+    public int? countSolid() {
+        if (grid == null) {
+            return null;
+        }
+
+        return grid.Cast<byte>().Count(x => x != 0);
+    }
+
     private SteamPos(bool isFullBlock, int x, int y, int z) : base(x, y, z)
     {
         this.isFullBlock = isFullBlock;
+        this.grid = null;
     }
 
     public static SteamPos FromBlockPos(bool isFullBlock, BlockPos pos)
