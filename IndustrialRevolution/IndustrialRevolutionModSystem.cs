19c9ed8fd0fa757236419204dad88987b0c15fc9
teodord25 <djuric.teodor25@gmail.com>
2026-01-12 17:32:35 +0100

    refactor(modsystem): reogranize

--- IndustrialRevolution/IndustrialRevolutionModSystem.cs
+++ IndustrialRevolution/IndustrialRevolutionModSystem.cs
@@ -20,13 +20,21 @@ public class IndustrialRevolutionModSystem : ModSystem
         api.Logger.Event($"IR Logger created on {api.Side}");
     }
 
+    private void RegisterComplexBlocks(ICoreAPI api) {
+        api.RegisterBlockClass(Mod.Info.ModID + "." + "blockboiler", typeof(BlockBoiler));
+    }
+
+    private void RegisterComplexEntities(ICoreAPI api) {
+        api.RegisterEntity(Mod.Info.ModID + "." + "entitysteam", typeof(EntitySteam));
+    }
+
+    private void RegiterBlockEntities(ICoreAPI api) {
+        api.RegisterBlockEntityClass(Mod.Info.ModID + "." + "entityboiler", typeof(BlockEntityBoiler));
+    }
+
     public override void Start(ICoreAPI api)
     {
         base.Start(api);
-
-        api.RegisterBlockClass(Mod.Info.ModID + "." + "blockboiler", typeof(BlockBoiler));
-        api.RegisterEntity(Mod.Info.ModID + "." + "entitysteam", typeof(EntitySteam));
-        api.RegisterBlockEntityClass(Mod.Info.ModID + "." + "entityboiler", typeof(BlockEntityBoiler));
 
         api.World.Logger.Event("started 'Industrial Revolution' mod");
     }
